cc97a899a8ebb227483051e663ab100407cbd323
Eric Friedrich <44412556+FoxFireShot@users.noreply.github.com>
2023-07-10 16:28:42 -0300

    10 formas de hello world

---
+++ src/script.js
@@ -0,0 +1,43 @@
+var container = document.getElementById("container");
+
+var div;
+
+div = "<div class='world'>Hello World</div>";
+
+container.innerHTML += div;
+
+div = "<span class='world'>Hello World</span>";
+
+container.innerHTML += div;
+
+div = "<h1 class='world'>Hello World</h1>";
+
+container.innerHTML += div;
+
+div = "<p class='world'>Hello World</p>";
+
+container.innerHTML += div;
+
+div = "<strong class='world'>Hello World</strong> <p>";
+
+container.innerHTML += div;
+
+div = "<em class='world'>Hello World</em>";
+
+container.innerHTML += div;
+
+div = "<blockquote class='world'>Hello World</blockquote>";
+
+container.innerHTML += div;
+
+div = "<pre class='world'>Hello World</pre>";
+
+container.innerHTML += div;
+
+div = "<code class='world'>Hello World</code> <p>";
+
+container.innerHTML += div;
+
+div = "<kbd class='world'>Hello World</kbd>";
+
+container.innerHTML += div;
